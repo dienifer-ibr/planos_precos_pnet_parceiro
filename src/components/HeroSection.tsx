ceccda6ce5e330a7159144893d319e3b085bc416
dienifergraziela <dienifergrazielaripl@gmail.com>
2025-09-04 14:36:58 -0300

    Atualização 04/09

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,16 +1,17 @@
-import parobe from "@/assets/parobe.jpg";
+import parobe from "@/assets/parobe.png";
 import carousel1 from "@/assets/carousel-1.png";
 import carousel2 from "@/assets/carousel-2.png";
 import carousel3 from "@/assets/carousel-3.png";
 import carousel4 from "@/assets/carousel-4.png";
-import logo from "@/assets/logo-branca.png";
+import logo from "@/assets/logo-simples.png";
+import premio from "@/assets/premio.png";
 
 const HeroSection = () => {
   const awardImages = [
-    { src: carousel1, alt: "Prêmio - Maior Satisfação" },
-    { src: carousel2, alt: "Prêmio - Melhor Provedor" },
-    { src: carousel3, alt: "Prêmio - Melhor Internet Gamer" },
-    { src: carousel4, alt: "Prêmio - Melhor Velocidade" },
+    { src: carousel1, alt: "Prêmio - Melhor Provedor" },
+    { src: carousel2, alt: "Prêmio - Melhor Velocidade" },
+    { src: carousel3, alt: "Prêmio - Maior Satisfação" },
+    { src: carousel4, alt: "Prêmio - Melhor Internet Gamer" },
   ];
 
   return (
@@ -18,20 +19,26 @@ const HeroSection = () => {
       className="relative min-h-screen flex flex-col items-center justify-start overflow-hidden bg-cover bg-center"
       style={{ backgroundImage: `url(${parobe})` }}
     >
-      <div className="absolute inset-0 bg-blue-700/80" />
+      <div className="absolute inset-0 bg-blue-800/80" />
 
       <div className="relative z-10 w-full flex justify-center pt-20">
         <img src={logo} alt="ParanhanaNet" className="w-56 lg:w-68" />
       </div>
 
       <div className="relative z-10 flex-1 flex items-center w-full max-w-7xl px-4 sm:px-6 lg:px-8 py-16">
-        <div className="grid lg:grid-cols-2 gap-12 items-center w-full">
-          <div className="text-white space-y-8">
-
-            <div className="space-y-6 text-lg leading-relaxed opacity-90">
-              <p>
-                A <strong>MAIS VELOZ DO PARANHANA</strong> <br />
-                <strong>ENTRE AS 3 MELHORES DO ESTADO DO RS</strong>
+        <div className="grid grid-cols-2 gap-12 items-center w-full">
+          <div className="flex flex-col items-center text-center text-white space-y-8 -mt-16">
+            <div className="opacity-90">
+              <img
+                src={premio}
+                alt="prêmio"
+                className="w-68 mb-12"
+              />
+              <h2>
+                <strong>A MAIS VELOZ DO VALE DO PARANHANA</strong>
+              </h2>
+              <p className="text-[10px] leading-none mt-2">
+                ENTRE AS 3 MELHORES DO ESTADO DO RS
               </p>
             </div>
           </div>
