347ecccc6a1c78d6382d90de6810c8351bcbaba0
Dienifer <dienifer.ibrsistemas@gmail.com>
2025-09-19 16:41:09 -0300

    Atualização 19/09 - 13

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -107,37 +107,3 @@ const FormularioFront: React.FC = () => {
 };
 
 export default FormularioFront;
-
-
-
-
-
-//--------------------------------------------------/PARCEIRO-------------------------------------------------------------
-// import React, { useState, useEffect } from 'react';
-
-// const FormularioFront: React.FC = () => {
-//   const [iframeSrc, setIframeSrc] = useState('https://app.mailingboss.com/lists/68cc3a561e4e2/subscribe');
-
-//   useEffect(() => {
-//     const params = new URLSearchParams(window.location.search);
-//     const idParam = params.get("id");
-
-//     if (idParam) {
-//       setIframeSrc(`https://app.mailingboss.com/lists/68cc3a561e4e2/subscribe?id=${idParam}`);
-//     }
-//   }, []);
-
-  
-
-//   return (
-//     <div style={{ maxWidth: "800px", margin: "0 auto" }}>
-//       <iframe
-//         src={iframeSrc}
-//         style={{ width: "100%", height: "500px", border: "0", display: "block", marginTop: "20px" }}
-//         scrolling="no"
-//       ></iframe>
-//     </div>
-//   );
-// };
-
-// export default FormularioFront;
